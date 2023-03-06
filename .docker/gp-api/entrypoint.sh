d67ad951bee8e60b49a58829c6214343b876bad0
Vinicius Ribeiro <vncs_ribeiro@hotmail.com>
2023-03-06 00:51:10 -0300

    Created user model; Installed gems rack-cors and annotate

--- .docker/gp-api/entrypoint.sh
+++ .docker/gp-api/entrypoint.sh
@@ -3,9 +3,6 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /gp-api/tmp/pids/server.pid
-if [ "${*}" == "./bin/rails server"]; then
-    ./bin/rails db:prepare
-fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
---
+++ gp-api/app/models/user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Schema Information
+#
+# 
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+end
