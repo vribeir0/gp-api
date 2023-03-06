58c45816b8e3249957ea13e67e8025a6903a1f9a
Vinicius Ribeiro <vncs_ribeiro@hotmail.com>
2023-03-06 17:57:27 -0300

    Created the schema information of the models

--- gp-api/app/models/user.rb
+++ gp-api/app/models/user.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 # Schema Information
 #
-# 
+#
 class User < ApplicationRecord
   before_save :downcase_email
 
