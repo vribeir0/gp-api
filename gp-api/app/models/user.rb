0c9ee069520dc25c86dfc6623b29d83db79d6d4e
Vinicius Ribeiro <vncs_ribeiro@hotmail.com>
2023-03-06 22:58:13 -0300

    feat: improve user model, install bcrypt gem and create confirmation and password columns on Users

--- gp-api/app/models/user.rb
+++ gp-api/app/models/user.rb
@@ -17,9 +17,21 @@
 #
 #
 class User < ApplicationRecord
+  CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+
+  has_secure_password
+
   before_save :downcase_email
 
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+
+  def confirm!
+    update_column(confirmed_at: Time.current)
+  end
+
+  def confirmed?
+    confirmed_at.present?
+  end
 
   private
 
--- gp-api/db/schema.rb
+++ gp-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_020857) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_014706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_020857) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
